b07db03cc580e134e94225019899c6f1737cd94d
Daniel Junior <jrzabott@gmail.com>
2022-03-18 11:29:35 +0000

    WrapperCheck now calculates sum and avg of an ArrayList.

--- src/WrapperCheck.java
+++ src/WrapperCheck.java
@@ -1,7 +1,25 @@
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WrapperCheck {
     public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(3);
+        nums.add(5);
+        nums.add(6);
+        nums.add(7);
+
+        int sum = 0;
+        for (Integer n: nums) {
+            sum += n;
+        }
+
+
         System.out.println("Daniel Junior");
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Avg = " + sum/nums.size());
     }
 }
