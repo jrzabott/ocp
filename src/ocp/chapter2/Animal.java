0972c4ccc5a5d5214882c3a3a9426f7b5e7b7ea6
Daniel Junior <jrzabott@gmail.com>
2022-04-09 23:43:29 +0100

    Immutable object and cloning.

--- src/ocp/chapter2/Animal.java
+++ src/ocp/chapter2/Animal.java
@@ -1,32 +1,63 @@
 package ocp.chapter2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Animal {
 
-    private String species;
-    private int age;
+    private final List<String> favoriteFoods;
+    private final String species;
+    private final int age;
 
-    public Animal(String species){
-        this.setSpecies(species);
+    public Animal(String species, int age, List<String> favoriteFoods) {
+        if (favoriteFoods == null) {
+            throw new IllegalArgumentException("Favorite foods is required");
+        }
+        this.favoriteFoods = new ArrayList<>(favoriteFoods);
+
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be negative number");
+        }
+        this.age = age;
+
+        if (species == null || species.trim().isEmpty()) {
+            throw new IllegalArgumentException("Species cannot be empty");
+        }
+        this.species = species;
+    }
+
+    // Modifying an immutable object.
+    public static void main(String[] args) {
+        Animal lion = new Animal("lion", 5, Arrays.asList("meat", "more meat"));
+
+        List<String> favoriteFoods = new ArrayList<>(lion.getFavoriteFoods());
+
+        Animal updatedLion = new Animal(lion.getSpecies(), lion.getAge() + 1, favoriteFoods);
+
+        System.out.println(lion);
+        System.out.println(updatedLion);
+    }
+
+    public List<String> getFavoriteFoods() {
+//        return favoriteFoods; // make the class mutable
+        return new ArrayList<>(favoriteFoods);
     }
 
     public String getSpecies() {
         return species;
     }
 
-    private void setSpecies(String species) {
-        if(species == null || species.isEmpty()){
-            throw new IllegalArgumentException("Species is required");
-        }
-        this.species = species;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        if(age < 0){
-            throw new IllegalArgumentException("Age cannot be negative number");
-        }
-        this.age = age;
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "favoriteFoods=" + favoriteFoods +
+                ", species='" + species + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
