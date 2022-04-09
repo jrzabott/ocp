30f1b35e1c989581c132ceb09541dbbaf4c76964
Daniel Junior <jrzabott@gmail.com>
2022-04-09 23:26:43 +0100

    Singleton quick overview

---
+++ src/ocp/chapter2/StaffRegister.java
@@ -0,0 +1,16 @@
+package ocp.chapter2;
+
+public class StaffRegister {
+    private static final StaffRegister instance;
+
+    static {
+        instance = new StaffRegister();
+        // Perform additional steps here
+    }
+
+    private StaffRegister() {}
+
+    public static StaffRegister getInstance() {
+        return instance;
+    }
+}
