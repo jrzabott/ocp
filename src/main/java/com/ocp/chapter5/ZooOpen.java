dc2bae721a91e9a9e98afcfa6b2314fdef327ed5
Daniel Junior <jrzabott@gmail.com>
2022-05-26 18:39:44 +0100

    Using Properties class, and printing a default value for nonexistent values.

--- src/main/java/com/ocp/chapter5/ZooOpen.java
+++ src/main/java/com/ocp/chapter5/ZooOpen.java
@@ -1,6 +1,7 @@
 package com.ocp.chapter5;
 
 import java.util.Locale;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class ZooOpen {
@@ -10,6 +11,32 @@ public class ZooOpen {
         printProperties(us);
         System.out.println();
         printProperties(france);
+
+        Properties props = loadBundleToPropertiesAndPrintIt();
+
+        printingANonExistentProperty(props);
+    }
+
+    private static void printingANonExistentProperty(Properties props) {
+        System.out.println();
+        System.out.println("Trying to print an nonexistent prop, and print a default value");
+        final String property = props.getProperty("nonExistent", "default value");
+        System.out.println("Non existent property <" + property + ">");
+    }
+
+    private static Properties loadBundleToPropertiesAndPrintIt() {
+        Properties props = loadResourceBundleToProperties();
+        System.out.println();
+        System.out.println("Printing all props, using a stream.forEach()");
+        props.forEach((key, value) -> System.out.println(key + ": " + value));
+        return props;
+    }
+
+    private static Properties loadResourceBundleToProperties() {
+        Properties props = new Properties();
+        ResourceBundle rb = ResourceBundle.getBundle("Zoo");
+        rb.keySet().forEach(key -> props.put(key, rb.getString(key)));
+        return props;
     }
 
     private static void printProperties(Locale locale) {
