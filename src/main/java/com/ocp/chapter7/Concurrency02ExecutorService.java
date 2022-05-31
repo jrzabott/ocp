56e580dbdce3e9a3f5f720447b073d069c969c49
Daniel Junior <jrzabott@gmail.com>
2022-05-31 15:26:18 +0100

    Concurrency02ExecutorService - refactor

--- src/main/java/com/ocp/chapter7/Concurrency02ExecutorService.java
+++ src/main/java/com/ocp/chapter7/Concurrency02ExecutorService.java
@@ -5,6 +5,21 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Concurrency02ExecutorService {
+
+    // submitting a long running task
+    private static Runnable aLongTask = () -> {
+        try {
+            Thread.sleep(5000);
+            System.out.println("After 5 seconds");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    };
+
+    // submit a simple task
+    private static Runnable aShortTask = () -> System.out.println("Hello");
+
     public static void main(String[] args) throws InterruptedException {
         // create an ExecutorService
         ExecutorService service = null;
@@ -12,24 +27,12 @@ public class Concurrency02ExecutorService {
             // create a single thread pool
             service = Executors.newSingleThreadExecutor();
 
-            // submit a simple task
-            service.submit(() -> System.out.println("Hello"));
-
-            // submitting a long running task
-            final Future<?> submit = service.submit(() -> {
-                try {
-                    Thread.sleep(5000);
-                    System.out.println("After 5 seconds");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    Thread.currentThread().interrupt();
-                }
-            });
-
+            service.submit(aShortTask);
+            final Future<?> result = service.submit(aLongTask);
 
             // check if the task is done
             while (true) {
-                if (!submit.isDone()) {
+                if (!result.isDone()) {
                     System.out.println("Waiting");
                     Thread.sleep(1000);
                 } else {
