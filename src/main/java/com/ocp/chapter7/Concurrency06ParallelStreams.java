60c56db0fe8af57610d8d7ac52eb22f798e356a9
Daniel Junior <jrzabott@gmail.com>
2022-06-02 23:55:39 +0100

    Concurrency06ParallelStreams - refactor

--- src/main/java/com/ocp/chapter7/Concurrency06ParallelStreams.java
+++ src/main/java/com/ocp/chapter7/Concurrency06ParallelStreams.java
@@ -7,16 +7,17 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Concurrency06ParallelStreams {
+
+    protected static final List<Integer> LIST = IntStream.range(0, 1000000).boxed().collect(Collectors.toList());
+
     public static void main(String[] args) {
-        final List<Integer> aList = IntStream.range(0, 1000000).boxed().collect(Collectors.toList());
+        performParallelCalculation(LIST);
+        performSequentialCalculation(LIST);
+    }
 
-        Instant start = Instant.now();
-        final long[] sumParallel = new long[1];
-        System.out.println("Parallel Stream");
-        sumParallel[0] = aList.parallelStream().reduce(0, (a, b) -> {a += b; return a;} );
-        Duration timeElapsed = Duration.between(start, Instant.now());
-        System.out.println("\nTime elapsed: " + timeElapsed.toMillis() + " ms" + "\nSum: " + sumParallel[0]);
-
+    private static void performSequentialCalculation(List<Integer> aList) {
+        Duration timeElapsed;
+        Instant start;
         System.out.println("\n==============================");
         start = Instant.now();
         final long[] sumSequential = new long[1];
@@ -24,6 +25,15 @@ public class Concurrency06ParallelStreams {
         sumSequential[0] = aList.stream().reduce(0, (a, b) -> {a += b; return a;} );
         timeElapsed = Duration.between(start, Instant.now());
         System.out.println("\nTime elapsed: " + timeElapsed.toMillis() + " ms" + "\nSum: " + sumSequential[0]);
+    }
+
+    private static void performParallelCalculation(List<Integer> aList) {
+        Instant start = Instant.now();
+        final long[] sumParallel = new long[1];
+        System.out.println("Parallel Stream");
+        sumParallel[0] = aList.parallelStream().reduce(0, (a, b) -> {a += b; return a;} );
+        Duration timeElapsed = Duration.between(start, Instant.now());
+        System.out.println("\nTime elapsed: " + timeElapsed.toMillis() + " ms" + "\nSum: " + sumParallel[0]);
     }
 
 
