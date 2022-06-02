6653e922b752c81468bc7d102001832cde98f131
Daniel Junior <jrzabott@gmail.com>
2022-06-03 00:48:52 +0100

    Concurrency10CyclicBarrier -> now using cyclic barrier and two cleaning cycles.

--- src/main/java/com/ocp/chapter7/Concurrency10CyclicBarrier.java
+++ src/main/java/com/ocp/chapter7/Concurrency10CyclicBarrier.java
@@ -1,5 +1,7 @@
 package com.ocp.chapter7;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -9,12 +11,24 @@ public class Concurrency10CyclicBarrier {
     Let's suppose 4 pen managers have to clean the Lion's pen. Oviously, they can't do that, while the lion in inside.
     How to represent this limitation using a CyclicBarrier?
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         LionPenManager manager = new LionPenManager();
+        CyclicBarrier removeAnimals = new CyclicBarrier(4);
+        CyclicBarrier cleanPen = new CyclicBarrier(4, () -> System.out.println("Lion's pen is clean"));
+
         ExecutorService service = Executors.newFixedThreadPool(4);
         for (int i = 0; i < 4; i++) {
-            service.submit(manager::performTasks);
+            service.submit(() -> manager.performTasks(removeAnimals, cleanPen));
         }
+
+        Thread.sleep(1000);
+        System.out.println("Rest a little...");
+        Thread.sleep(1000);
+        System.out.println("Resuming...");
+        for (int i = 0; i < 4; i++) {
+            service.submit(() -> manager.performTasks(removeAnimals, cleanPen));
+        }
+
         service.shutdown();
     }
 
@@ -34,9 +48,17 @@ class LionPenManager {
         System.out.println("Adding animals");
     }
 
-    public void performTasks() {
-        removeAnimals();
-        cleanPen();
-        addAnimals();
+    public void performTasks(CyclicBarrier barrier1, CyclicBarrier barrier2) {
+
+        try {
+            removeAnimals();
+            barrier1.await();
+            cleanPen();
+            barrier2.await();
+            addAnimals();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
     }
 }
