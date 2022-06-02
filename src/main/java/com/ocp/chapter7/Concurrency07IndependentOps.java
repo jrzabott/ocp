16ac35741068fc5ddb33acfcd07e89a5f83f6b0d
Daniel Junior <jrzabott@gmail.com>
2022-06-03 00:06:36 +0100

    Concurrency07IndependentOps - added ordered and unordered printing.

--- src/main/java/com/ocp/chapter7/Concurrency07IndependentOps.java
+++ src/main/java/com/ocp/chapter7/Concurrency07IndependentOps.java
@@ -1,12 +1,36 @@
 package com.ocp.chapter7;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Concurrency07IndependentOps {
     public static void main(String[] args) {
         printStringsInParallel(); // notice the order of printing is not guaranteed
+        printIntsOrderedAndUnordered();
 
+
+    }
+
+    private static void printIntsOrderedAndUnordered() {
+        List<Integer> data = Collections.synchronizedList(new ArrayList<>());
+        final List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("ForEach Ordered");
+        integers.parallelStream()
+                .map(i -> {
+                    data.add(i); // AVOID STATEFUL OPERATIONS AT ALL COSTS
+                    return i;
+                })
+                .forEachOrdered(i -> System.out.print(i + " "));
+
+        System.out.println("\nForEach Unordered");
+        integers.parallelStream()
+                .map(i -> {
+                    data.add(i); // AVOID STATEFUL OPERATIONS AT ALL COSTS
+                    return i;
+                })
+                .forEach(i -> System.out.print(i + " "));
     }
 
     private static void printStringsInParallel() {
